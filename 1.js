e2b93137d80827c4f0258be5e3238a8a48410e4a
Kaushik Rajesh Prabhu Nerurkar <21104016.kaushik.prabhunerurkar@gmail.com>
2025-06-16 17:28:18 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,15 +1,10 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-})
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url)
+    const target = 'https://chatgpt.com/' // 🔁 Replace with your target site
 
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  const target = 'https://chatgpt.com/'  // Replace with the blocked domain
-  const newUrl = target + url.pathname + url.search
-  const modifiedRequest = new Request(newUrl, {
-    method: request.method,
-    headers: request.headers,
-    body: request.body
-  })
-  return fetch(modifiedRequest)
+    const targetUrl = target + url.pathname + url.search
+    const modifiedRequest = new Request(targetUrl, request)
+    return fetch(modifiedRequest)
+  }
 }
